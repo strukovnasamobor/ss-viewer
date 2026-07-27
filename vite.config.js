b33d4d1973fba6a6ee4becf3b994b3a69d57d495
ZDamijan <zlatko.damijanic@gmail.com>
2026-07-27 16:23:23 +0000

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -24,6 +24,7 @@ export default defineConfig({
         display: "standalone",
         orientation: "portrait",
         start_url: "/",
+        scope: "/",
         icons: [
           { src: "icons/icon-192.png", sizes: "192x192", type: "image/png" },
           { src: "icons/icon-512.png", sizes: "512x512", type: "image/png" },
