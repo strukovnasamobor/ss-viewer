8bce35fdad2a629cca0110026655effeab565f49
ZDamijan <zlatko.damijanic@gmail.com>
2026-08-23 10:23:41 +0200

    added android-pwa

---
+++ android-pwa/app/src/main/java/com/strukovnasamobor/ssviewer_ucenici/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package com.strukovnasamobor.ssviewer_ucenici.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
